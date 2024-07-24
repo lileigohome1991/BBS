5c0850f9c4749a43bae3f0be99bdc3960f900fd5
limuyi666 <1846352100@qq.com>
2024-07-24 16:27:05 +0000

    用户话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,11 +37,11 @@
             <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" id="logout" href="#">
-                <form action="{{ route('logout') }}" method="POST">
-                {{ csrf_field() }}
-                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
                 </form>
-            </a>
+              </a>
             </div>
         </li>
         @endguest
