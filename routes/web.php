ded0b2e3d2286f084f80ade412db4172049d95c7
limuyi666 <1846352100@qq.com>
2024-07-25 08:40:33 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::put('/users/{user}', 'UsersController@update')->name('users.update');
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
-
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
