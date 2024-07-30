e66c883eb61f76fcda8275deda5b896bd0eec107
vagrant <vagrant@homestead>
2024-07-30 04:31:49 +0000

    默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -22,7 +22,7 @@ class UserObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+            $user->avatar = 'http://gips3.baidu.com/it/u=3886271102,3123389489&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=960';
         }
     }
 }
